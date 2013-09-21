d4d524b69c93e82645ed8bf658b2b707b8dd956a
Kevin Carter <cartkev@gmail.com>
2013-09-21 13:54:14 -0600

    Remove rouge comma

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Disseminate::Application.routes.draw do
-  match "/auth/:provider/callback" => "sessions#create", via: %i(get, post)
+  match "/auth/:provider/callback" => "sessions#create", via: %i(get post)
   match '/auth/failure' => 'sessions#failure', via: %i(get post)
   match '/logout' => 'sessions#destroy', via: %i(get delete), as: :logout
   get '/sessions/new'
